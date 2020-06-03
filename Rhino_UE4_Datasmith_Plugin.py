0d726c4d15ad483051f5defc93fc0580c452b326
rishipandeyBrunswick <rishi.pandey@brunswick.com>
2020-06-03 15:13:29 -0500

    Datasmith Element Created Successfully

--- Rhino_UE4_Datasmith_Plugin.py
+++ Rhino_UE4_Datasmith_Plugin.py
@@ -3,7 +3,7 @@ import unreal
 #Path of Rhino File
 rhino_file_path = "C:\Users\ijet\Desktop\Rhino_Files\405 CQ Website File.3dm"
 #Initializing Datasmith Element
-datasmith_file = unreal.DataSmithSceneElement.construct_datasmith_scene_from_file(rhino_file_path)
+datasmith_file = unreal.DatasmithSceneElement.construct_datasmith_scene_from_file(rhino_file_path)
 
 if datasmith_file == None:
     print "Failed to Load Rhino File as Datasmith Element"
