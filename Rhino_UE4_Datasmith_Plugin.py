74721f6bc50775fd81b2c27b08d96cf6192fb440
rishipandeyBrunswick <rishi.pandey@brunswick.com>
2020-06-03 15:07:29 -0500

    Testing Datasmith Element Creation
    Testing whether the Datasmith element is successfully created or not.

--- Rhino_UE4_Datasmith_Plugin.py
+++ Rhino_UE4_Datasmith_Plugin.py
@@ -1,3 +1,13 @@
+#BC iJet Lab's Rhino Unreal Engine Import Plugin
 import unreal
-#Path and Destination Folder
+#Path of Rhino File
+rhino_file_path = "C:\Users\ijet\Desktop\Rhino_Files\405 CQ Website File.3dm"
+#Initializing Datasmith Element
+datasmith_file = unreal.DataSmithSceneElement.construct_datasmith_scene_from_file(rhino_file_path)
+
+if datasmith_file == None:
+    print "Failed to Load Rhino File as Datasmith Element"
+    quit()
+else:
+    print "Datasmith Element Successfully Created!"
 
