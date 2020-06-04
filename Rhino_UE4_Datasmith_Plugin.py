ad3c11a1c2b254ff5bdc49cb18e08c1a8cfac4a1
rishipandeyBrunswick <rishi.pandey@brunswick.com>
2020-06-04 16:19:04 -0500

    Import Path Incorrect

--- Rhino_UE4_Datasmith_Plugin.py
+++ Rhino_UE4_Datasmith_Plugin.py
@@ -22,6 +22,7 @@ if datasmith_file is None:
     print "Failed to Load Rhino File as Datasmith Element"
     quit()
 
+#i think there example is outdated, we will have to import first before handling any meshes
 
 #load the meshes, if you get a null static mesh add it to a list ot remove or directly remove it
 #good way to describe process: import data, delete small objectds and null static meshes,
@@ -32,19 +33,20 @@ if datasmith_file is None:
 #create a new folder in the content folder for everything
 #thinking we have to load the scene first before accessing meshes and stuff
 
-# import_base_options = unreal.DatasmithImportBaseOptions(scene_handling=unreal.DatasmithImportScene.CURRENT_LEVEL, 
-#                                                     include_geometry=True, 
-#                                                     include_material=False, 
-#                                                     include_light=False, 
-#                                                     include_camera=False, 
-#                                                     include_animation=False, 
-#                                                     asset_options=[], 
-#                                                     static_mesh_options=[unreal.DatasmithImportLightmapMin.LIGHTMAP_128, unreal.DatasmithImportLightmapMax.LIGHTMAP_512, True, True])
+import_base_options = unreal.DatasmithImportBaseOptions(scene_handling=unreal.DatasmithImportScene.CURRENT_LEVEL, 
+                                                    include_geometry=True, 
+                                                    include_material=False, 
+                                                    include_light=False, 
+                                                    include_camera=False, 
+                                                    include_animation=False, 
+                                                    asset_options=[], 
+                                                    static_mesh_options=[unreal.DatasmithImportLightmapMin.LIGHTMAP_128, unreal.DatasmithImportLightmapMax.LIGHTMAP_512, True, True])
 
 import_options = datasmith_file.get_options()
-print(import_options)
 
+import_options.base_options = import_base_options
 
-# import_options.base_options = import_base_options
+destination_folder = "Game/RhinoPythonExample"
 
-# datasmith_file.import_scene("C:\\Users\\ijet\\Documents\\Unreal Engine\\RhinoUnrealTest\\Content\\RhinoAsset2")
+datasmith_file.import_scene(destination_folder)
+
