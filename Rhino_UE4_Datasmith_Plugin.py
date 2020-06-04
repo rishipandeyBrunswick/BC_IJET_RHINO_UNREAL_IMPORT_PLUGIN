73c41bda06844c56f46dda68d9c621b2dcdfdad0
rishipandeyBrunswick <rishi.pandey@brunswick.com>
2020-06-04 11:41:40 -0500

    debugging import

--- Rhino_UE4_Datasmith_Plugin.py
+++ Rhino_UE4_Datasmith_Plugin.py
@@ -3,13 +3,15 @@ import unreal
 #Path of Rhino File
 rhino_file_path = "C:\\Users\\ijet\\Desktop\\Rhino_Files\\test.3dm"
 #Initializing Datasmith Element
+#i feel like it isnt working because the cad importer plugin isnt enabled in this action
+#like normally we would have the cad plugin enabled AND datasmith enabled
+#maybe that is why it isnt reading the file
 datasmith_file = unreal.DatasmithSceneElement.construct_datasmith_scene_from_file(rhino_file_path)
 
 if datasmith_file is None:
     print "Failed to Load Rhino File as Datasmith Element"
     quit()
-else: 
-    print "file loading successful" 
+
 
 #load the meshes, if you get a null static mesh add it to a list ot remove or directly remove it
 #good way to describe process: import data, delete small objectds and null static meshes,
@@ -17,7 +19,6 @@ else:
 #merge objects (stiching and tesselation and such)
 #materialize objects (S.T. extra data from rhino team)
 #export as asset! 
-
-mesh_list = datasmith_file.get_all_mesh_actors()
-
-print mesh_list
+#create a new folder in the content folder for everything
+meshes = datasmith_file.get_all_mesh_actors()
+print meshes
